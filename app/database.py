bf742d80dafef396cba3302361e0451b36401e91
RaviTwari03 <ravirkt0@gmail.com>
2026-02-21 15:39:30 +0530

    changes done

--- app/database.py
+++ app/database.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = MongoClient(os.getenv("mongodb+srv://rt9593878_db_user:<JOnykOk3SERWwK1q>@taskflowapicluster0.9nbp6wq.mongodb.net/?appName=TaskFlowAPICluster0"))
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://rt9593878_db_user:<JOnykOk3SERWwK1q>@taskflowapicluster0.9nbp6wq.mongodb.net/?appName=TaskFlowAPICluster0")
+
+client = MongoClient(MONGO_URL)
 db = client["taskflow"]
-collection = db["tasks"]
+users_collection = db["users"]
+products_collection = db["products"]
